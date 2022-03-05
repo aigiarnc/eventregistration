238f65904560f7334c416d7db180e0788942f69e
aigiarnc <aigiarn.cheuk@gmail.com>
2022-03-05 16:46:15 -0500

    removed ss

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/EventRegistrationRepository.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/EventRegistrationRepository.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import ca.mcgill.ecse321.eventregistration.model.Person;
 import ca.mcgill.ecse321.eventregistration.model.Event;
+import ca.mcgill.ecse321.eventregistration.model.Person;
 
 @Repository
 public class EventRegistrationRepository {
@@ -27,13 +27,13 @@ public class EventRegistrationRepository {
 		entityManager.persist(p);
 		return p;
 	}
-
+	
 	@Transactional
 	public Person getPerson(String name) {
 		Person p = entityManager.find(Person.class, name);
 		return p;
 	}
-
+	
 	@Transactional
 	public Event createEvent(String name, Date date, Time startTime, Time endTime) {
 		Event e = new Event();
@@ -44,7 +44,7 @@ public class EventRegistrationRepository {
 		entityManager.persist(e);
 		return e;
 	}
-
+	
 	@Transactional
 	public Event getEvent(String name) {
 		Event e = entityManager.find(Event.class, name);
@@ -58,5 +58,6 @@ public class EventRegistrationRepository {
 		List<Event> resultList = q.getResultList();
 		return resultList;
 	}
-
+	
+	
 }
