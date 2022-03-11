867e0b58ae0ccc92ff38e7cf858065629b0f37da
aigiarnc <aigiarn.cheuk@gmail.com>
2022-03-11 17:52:59 -0500

    fix build error

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/PersonRepository.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dao/PersonRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.eventregistration.model.Person;
 
+//@RepositoryRestResource(collectionResourceRel = "person_data", path = "person_data")
 public interface PersonRepository extends CrudRepository<Person, String>{
 
 	Person findPersonByName(String name);
