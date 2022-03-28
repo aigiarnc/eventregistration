03f2515ed7160da7ad4624ec5528cc3a7908f89d
aigiarnc <aigiarn.cheuk@gmail.com>
2022-03-28 01:47:08 -0400

    Create Tutorial.vue

---
+++ EventRegistration-Frontend/src/components/Tutorial.vue
@@ -0,0 +1,38 @@
+<template>
+</template>
+<script>
+</script>
+<style>
+</style>
+
+<template>
+  <div id="eventregistration">
+    <h2>People</h2>
+    <table>
+      <tr>
+          <td>John</td>
+          <td>Event to attend</td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Person Name">
+          </td>
+          <td>
+              <button>Create</button>
+          </td>
+      </tr>
+    </table>
+    <p>
+      <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+//write css code directly in tag
+<style>
+  #eventregistration {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    background: #f2ece8;
+  }
+</style>
